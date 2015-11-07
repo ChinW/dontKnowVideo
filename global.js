b60f96072a7146d12bad7d1151656d0357f602a4
CHIBANG QN <contractswif@hotmail.com>
2015-11-08 00:50:14 +0800

    no message

--- global.js
+++ global.js
@@ -1,15 +1,27 @@
-function saveChanges(target,saveName) {
+function saveVideoModeChanges() {
     // 获取表单中保存的值。
-    var theValue = $(target).prop('checked');
-    // 确保包含代码。
+    var theValue = $("#switch-background").prop('checked');
+    console.log("设置已经保存:"+theValue);
     if (theValue === undefined ) {
       $('.cookie.nag').nag('show').html("保存出错，请重新打开应用再试");
       return;
     }
-    // 使用 Chrome 扩展程序的存储 API 保存它。
-    chrome.storage.local.set({saveName: theValue}, function() {
+    chrome.storage.local.set({'videoMode': theValue}, function() {
       // 通知保存完成。
-      $("#status").html("设置已经保存:"+saveName+":"+theValue);
+      // $("#status").html("设置已经保存:"+theValue);
+    });
+}
+function saveRepeatModeChanges() {
+    // 获取表单中保存的值。
+    var theValue = $("#repeat-background").prop('checked');
+    console.log("设置已经保存:"+theValue);
+    if (theValue === undefined ) {
+      $('.cookie.nag').nag('show').html("保存出错，请重新打开应用再试");
+      return;
+    }
+    chrome.storage.local.set({'repeatMode': theValue}, function() {
+      // 通知保存完成。
+      // $("#status").html("设置已经保存:"+theValue);
     });
 }
 $(function(){
@@ -17,30 +29,30 @@ $(function(){
 	$('.ui.checkbox').checkbox();
 	
 	$("#switch-background").bind("change",function(){
-		saveChanges("#switch-background","videoMode");
+		$("#status").html(1);
+		saveVideoModeChanges();
 	});
 
-	// $("#repeat-background").bind("change",function(){
-	// 	saveChanges("#repeat-background","repeatMode");
-	// });
-	
+	$("#repeat-background").bind("change",function(){
+		saveRepeatModeChanges();
+	});
+
 	var videoMode = "";
 	var repeatMode = "";
+
 	chrome.storage.local.get('videoMode', function (result) {
         videoMode = result.videoMode;
-        $("#status").html("videoMode2:"+result);
-        // if(videoMode){
-        //  	$("#status").html("videoMode2:"+videoMode);
-        // 	// $("#videoMode-checkbox").checkbox('check');
-        // }
+        if(videoMode){
+         	$("#status").html("videoMode2:"+videoMode);
+        	$("#videoMode-checkbox").checkbox('check');
+        }
     });
-    // chrome.storage.local.get('repeatMode', function (result) {
 
-    //     repeatMode = result.repeatMode;
-    //     $("#status").html("repeatMode2:"+repeatMode);
-    //     if(repeatMode){
-    //      	// $("#status").html("videoMode2:"+videoMode);
-    //     	$("#repeat-checkbox").checkbox('check');
-    //     }
-    // });
+    chrome.storage.local.get('repeatMode', function (result) {
+        videoMode = result.videoMode;
+        if(videoMode){
+         	$("#status").html("repeatMode:"+videoMode);
+        	$("#repeatMode-checkbox").checkbox('check');
+        }
+    });
 })
